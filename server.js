2f52509b303c4261c10b23a074ad7e0ccfa7c5dc
goodrichbrett <brettgoodrich@outlook.com>
2020-09-16 10:34:26 -0500

    remove all instances of favicon

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 require('dotenv').config();
@@ -10,25 +9,23 @@ require('./config/database');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 
-const cors = require('cors')
-
+const cors = require('cors');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, ()=> {
-  console.log(`Express is listening on port ${port}.`)
+app.listen(port, () => {
+	console.log(`Express is listening on port ${port}.`);
 });
