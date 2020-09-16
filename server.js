73fa3f1f4da2e9c2cc00fb1ff84cee4329846e56
goodrichbrett <brettgoodrich@outlook.com>
2020-09-16 10:32:16 -0500

    create landing page component file

--- server.js
+++ server.js
@@ -10,25 +10,23 @@ require('./config/database');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 
-const cors = require('cors')
-
+const cors = require('cors');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, ()=> {
-  console.log(`Express is listening on port ${port}.`)
+app.listen(port, () => {
+	console.log(`Express is listening on port ${port}.`);
 });
