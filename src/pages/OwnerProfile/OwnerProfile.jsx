95a1dda41ed73b506320a2c3aaad680efbe63a86
dscription <dscriptionplayground@gmail.com>
2020-09-18 14:14:05 -0400

    modify state in app.js, modify OwnerProfile component

--- src/pages/OwnerProfile/OwnerProfile.jsx
+++ src/pages/OwnerProfile/OwnerProfile.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
+import * as petAPI from '../../services/petService';
 
 class OwnerProfile extends Component {
   state = { 
     followedPets: []
    }
 
-  // [] Call petsAPI services to get all followed pets
 	async componentDidMount() {
-		const followedPets = await petsAPI.getPets(this.props.user.followedPets);
+		const followedPets = await petAPI.getPets(this.props.user.followedPets);
 		this.setState({followedPets});
     console.log(this.state)
   }
   render() { 
-    return (  );
+    return ( 
+    <> 
+    <h1>Owner Profile Page</h1>
+    </>
+     );
   }
 }
  
