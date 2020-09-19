cbe7cf6aa99e380e15b88e816431ce01517d044c
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-09-19 14:53:12 -0400

    add react route to add post

---
+++ src/pages/AddPost/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AddPost extends React.Component {
+    state = {  }
+    render() { 
+        return (
+            <>
+                <h1>Add Post</h1>
+            </> 
+        );
+    }
+}
+ 
+export default AddPost;
