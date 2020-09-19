861a5de037f4dc2c15cb10355453787ec9b9bdea
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-09-19 17:22:34 -0400

    add post front end

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './PetCard.css';
 import {
 	Card, CardImg, CardText, CardBody,
-	CardTitle, CardSubtitle, Button
+	CardTitle, Button
   } from 'reactstrap';
 
 const PetCard = ({ pet }) => {
