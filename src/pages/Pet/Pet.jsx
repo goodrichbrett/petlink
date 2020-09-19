202d6213715c1ae9581fb4c26517a8510b65d170
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-09-19 14:32:57 -0400

    style pet-card 2

--- src/pages/Pet/Pet.jsx
+++ src/pages/Pet/Pet.jsx
@@ -6,6 +6,7 @@ const Pet = (props) => {
 		<div>
 			<h1 style={{ margin: '0 0 1em 0', textAlign: 'center' }}>{props.location.state.pet.alias}</h1>
 			<PetCard pet={props.location.state.pet} />
+			<h1 style={{marginTop: '1em', marginBottom: '1em', textAlign: 'center' }}>Activity</h1>
 		</div>
 	);
 };
