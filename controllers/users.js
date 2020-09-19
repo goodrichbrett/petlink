52765eb31fffde7a2b3dc2deee30928ccd01af15
goodrichbrett <brettgoodrich@outlook.com>
2020-09-19 12:45:18 -0500

    add update user functions

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,7 @@ const User = require('../models/user');
 module.exports = {
 	index,
 	showProfile,
+	update,
 };
 
 function index(req, res) {
@@ -15,4 +16,12 @@ function showProfile(req, res) {
 	User.findById(req.params.id).then((userInfo) => {
 		res.json(userInfo);
 	});
+}
+
+function update(req, res) {
+	User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+		(user) => {
+			res.status(200).json(user);
+		}
+	);
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,27 @@
-import tokenService from "../services/tokenService";
-const BASE_URL = "/api/users/";
+import tokenService from '../services/tokenService';
+const BASE_URL = '/api/users/';
 
 export function getAllUsers() {
-  return fetch(
-    BASE_URL,
-    {
-      headers: { Authorization: "Bearer " + tokenService.getToken() },
-    },
-    { mode: "cors" }
-  ).then((res) => res.json());
+	return fetch(
+		BASE_URL,
+		{
+			headers: { Authorization: 'Bearer ' + tokenService.getToken() },
+		},
+		{ mode: 'cors' }
+	).then((res) => res.json());
+}
+
+export function update(user) {
+	return fetch(
+		`${BASE_URL}${user._id}`,
+		{
+			method: 'PUT',
+			headers: {
+				'content-type': 'application/json',
+				Authorization: 'Bearer ' + tokenService.getToken(),
+			},
+			body: JSON.stringify(user),
+		},
+		{ mode: 'cors' }
+	).then((res) => res.json());
 }
