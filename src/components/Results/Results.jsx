82a6cffc87246495bd14b80bfe830c79b3c75a94
dscription <dscriptionplayground@gmail.com>
2020-09-24 16:21:31 -0400

    fix map issue

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import SummaryCard from '../SummaryCard/SummaryCard';
+import React from "react";
+import SummaryCard from "../SummaryCard/SummaryCard";
 
 const Results = (props) => {
-    return ( <>{props.pets.map((pet, idx) => (
+  return (
+    <>
+      {props.pets.map((pet, idx) => (
         <SummaryCard key={idx} pet={pet} />
-    ))} 
-    </>);
-}
- 
+      ))}
+    </>
+  );
+};
+
 export default Results;
